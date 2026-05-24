97cc7f3f5ad6eb43a0119276dc36ccf2680e0c1c
himichael00 <michaelrio89@gmail.com>
2026-05-24 23:29:26 +0700

    EDIT: edit CORS for prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,10 @@ app = FastAPI(title="Portfolio API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://my-portofolio-virid-xi.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
